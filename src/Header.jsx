8e98f7a0e55060e08afcaf4b7eea3eafbffbfffa
anthonykravchishin <tonykravc53@gmail.com>
2026-04-23 22:17:46 -0700

    Added some styling

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,10 +3,10 @@ import './Header.css'
 
 export default function Header() {
     return (
-        <>
+        <div id="header">
             <h1>{data.label}</h1>
             <h2><i>"{data.tagline}"</i></h2>
             <p>Founded in {data.founded} at {data.city}</p>
-        </>
+        </div>
     )
 }
